ec93e9127477c0e5c5deb408e5332ec7d175c21f
Eugene Kurpilyansky <dembelz@yandex.ru>
2017-08-16 23:27:12 +0300

    command /set_region_chat

--- street_agitation_bot/bot.py
+++ street_agitation_bot/bot.py
@@ -1533,6 +1533,22 @@ def transfer_cube_to_event(bot, update, user_data, groups):
 def help(bot, update):
     update.message.reply_text('Help!', reply_markup=ReplyKeyboardRemove())
 
+
+def set_region_chat_command(bot, update, args):
+    telegram_user_id = update.effective_user.id
+    chat_id = update.effective_chat.id
+    if bot_settings.is_admin_user_id(telegram_user_id):
+        region_name = ' '.join(args[1:])
+        region = models.Region.find_by_name(region_name, telegram_user_id)
+        if not region:
+            bot.send_message(chat_id, "Регион '%s' не найден" % region_name)
+        elif args[0] == '0':
+            region.registrations_chat_id = chat_id
+            region.save()
+            bot.send_message(chat_id, "Привязан новый чат 'Регистрации' в регионе '%s'" % region_name)
+        else:
+            bot.send_message(chat_id, "Неизвестный номер чата (первый аргумент должен быть 0)")
+
     
 def send_bug_report(bot, update, user_data):
     state = models.ConversationState.objects.filter(key=update.effective_user.id).first()
@@ -1557,6 +1573,7 @@ def run_bot():
 
     # dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("set_region_chat", set_region_chat_command, pass_args=True))
 
     standard_callback_query_handler = CallbackQueryHandler(standard_callback)
 
